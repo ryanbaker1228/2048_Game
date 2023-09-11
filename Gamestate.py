2b2dfc0616a57e58d15a916adc3687df1bd0a87f
Ryan Baker <rbaker@xtier.com>
2023-09-10 23:44:19 -0400

    Added MakeMove function and GameOver function with key presses

--- Gamestate.py
+++ Gamestate.py
@@ -3,13 +3,6 @@ import random
 import math
 
 
-class Direction(Enum):
-    right = 0,
-    left = 1,
-    up = 2,
-    down = 3
-
-
 class Gamestate:
     def __init__(self):
         self.board = [[0, 0, 0, 0],
@@ -38,9 +31,87 @@ class Gamestate:
         while square_2 == square_1:
             square_2 = math.floor(random.random() * 16)
 
-        self.board[square_1 // 4][square_1 % 4] = initial_number_1
-        self.board[square_2 // 4][square_2 % 4] = initial_number_2
+        self.board[square_1 % 4][square_1 // 4] = initial_number_1
+        self.board[square_2 % 4][square_2 // 4] = initial_number_2
 
-    def MakeMove(self, direction: Direction):
-        pass
+    def MakeMove(self, direction: str):
+        if direction == 'right':
+            for row in self.board:
+                for tile in range(3, -1, -1):
+                    if tile == 3 or row[tile] == 0: continue
 
+                    for index in range(tile + 1, 4):
+                        if index == 4:
+                            break
+
+                        if row[index] == row[tile]:
+                            row[index] *= 2
+                            row[tile] = 0
+                            break
+
+                        elif row[index] != 0:
+                            row[index - 1] = row[tile]
+                            if index != tile + 1: row[tile] = 0
+                            break
+
+                        elif index == 3:
+                            row[index] = row[tile]
+                            row[tile] = 0
+                            break
+
+            rand_num = random.random()
+            if rand_num > 0.99:
+                number = 8
+            elif rand_num > 0.9:
+                number = 4
+            else:
+                number = 2
+
+            square = math.floor(random.random() * 16)
+            while self.board[square % 4][square // 4] > 0:
+                square = math.floor(random.random() * 16)
+
+            self.board[square % 4][square // 4] = number
+
+        elif direction == 'left':
+            for row in self.board:
+                row.reverse()
+
+            self.MakeMove('right')
+
+            for row in self.board:
+                row.reverse()
+
+        elif direction == 'up':
+            self.board = Transpose(self.board)
+            self.MakeMove('left')
+            self.board = Transpose(self.board)
+
+        elif direction == 'down':
+            self.board = Transpose(self.board)
+            self.MakeMove('right')
+            self.board = Transpose(self.board)
+
+    def GameOver(self):
+        board = self.board[0] + self.board[1] + self.board[2] + self.board[3]
+        if board.count(0) > 0: return False
+
+        for row in self.board:
+            for index in range(3):
+                if row[index] == row[index + 1]: return False
+
+        board = Transpose(self.board)
+        for row in board:
+            for index in range(3):
+                if row[index] == row[index + 1]: return False
+
+        return True
+
+def Transpose(matrix):
+    transposed = [[0] * 4 for _ in range(4)]
+
+    for i in range(4):
+        for j in range(4):
+            transposed[i][j] = matrix[j][i]
+
+    return transposed
