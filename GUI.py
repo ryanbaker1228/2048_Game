498c81dfa1e7eec0cb902714489c549d02082f2f
Ryan Baker <rbaker@xtier.com>
2023-09-10 22:47:16 -0400

    Finished drawing squares and added different colors

--- GUI.py
+++ GUI.py
@@ -4,19 +4,20 @@ import pygame as py
 
 
 square_colors = {
+    0: py.Color(40, 42, 58, 255),
     2: py.Color(250, 230, 85, 255),
     4: py.Color(214, 223, 78, 255),
-    8: py.Color(250, 230, 85, 255),
-    16: py.Color(250, 230, 85, 255),
-    32: py.Color(250, 230, 85, 255),
-    64: py.Color(250, 230, 85, 255),
-    128: py.Color(250, 230, 85, 255),
-    256: py.Color(250, 230, 85, 255),
-    512: py.Color(250, 230, 85, 255),
-    1024: py.Color(250, 230, 85, 255),
-    2048: py.Color(250, 230, 85, 255),
-    4096: py.Color(250, 230, 85, 255),
-    8192: py.Color(250, 230, 85, 255),
+    8: py.Color(177, 216, 85, 255),
+    16: py.Color(143, 205, 99, 255),
+    32: py.Color(115, 193, 114, 255),
+    64: py.Color(94, 179, 126, 255),
+    128: py.Color(80, 165, 134, 255),
+    256: py.Color(72, 150, 139, 255),
+    512: py.Color(67, 230, 85, 255),
+    1024: py.Color(64, 119, 139, 255),
+    2048: py.Color(62, 104, 138, 255),
+    4096: py.Color(62, 87, 136, 255),
+    8192: py.Color(65, 70, 130, 255),
 }
 
 
@@ -27,6 +28,7 @@ class GUI:
         self.border_size = (self.screen_size - self.square_size * 4) / 5
 
         self.window = py.display.set_mode((self.screen_size, self.screen_size))
+        self.window.fill((30, 31, 43, 255))
         self.font = py.font.Font('freesansbold.ttf', 40)
 
     def DrawGame(self, gamestate: Gamestate.Gamestate):
@@ -38,9 +40,12 @@ class GUI:
             centerY = row * self.square_size + (row + 1) * self.border_size
 
             number = gamestate.board[row][col]
-            color = py.Color(50, 200, 140, 255)
+            color = square_colors[number]
             black = py.Color(0, 0, 0, 255)
-            text = self.font.render(str(number), True, black, None)
+            if number > 0:
+                text = self.font.render(str(number), True, black, None)
+            else:
+                text  = self.font.render('', True, black, None)
 
             d_rect = py.Rect(centerX, centerY, self.square_size, self.square_size)
             py.draw.rect(self.window, color, d_rect)
--- Gamestate.py
+++ Gamestate.py
@@ -1,5 +1,13 @@
+from enum import Enum
 import random
 import math
+
+
+class Direction(Enum):
+    right = 0,
+    left = 1,
+    up = 2,
+    down = 3
 
 
 class Gamestate:
@@ -9,14 +17,30 @@ class Gamestate:
                       [0, 0, 0, 0],
                       [0, 0, 0, 0]]
 
-        rand_num = random.random()
-        if rand_num > 0.99:
-            initial_number = 8
-        elif rand_num > 0.9:
-            initial_number = 4
+        rand_num_1 = random.random()
+        rand_num_2 = random.random()
+        if rand_num_1 > 0.99:
+            initial_number_1 = 8
+        elif rand_num_1 > 0.9:
+            initial_number_1 = 4
         else:
-            initial_number = 2
+            initial_number_1 = 2
 
-        square = math.floor(random.random() * 16)
-        self.board[square // 4][square % 4] = initial_number
+        if rand_num_2 > 0.99:
+            initial_number_2 = 8
+        elif rand_num_2 > 0.9:
+            initial_number_2 = 4
+        else:
+            initial_number_2 = 2
+
+        square_1 = math.floor(random.random() * 16)
+        square_2 = math.floor(random.random() * 16)
+        while square_2 == square_1:
+            square_2 = math.floor(random.random() * 16)
+
+        self.board[square_1 // 4][square_1 % 4] = initial_number_1
+        self.board[square_2 // 4][square_2 % 4] = initial_number_2
+
+    def MakeMove(self, direction: Direction):
+        pass
 
